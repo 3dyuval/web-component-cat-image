17d8c41a8a1e3d20bc23a2af9e0df6b97a5b4858
Yuval.D <yuval.dikerman@rayzoneg.com>
2023-12-04 13:09:22 +0200

    init

---
+++ script.js
@@ -0,0 +1,18 @@
+import CatImage  from "./CatImage.js";
+
+// async function getCatImgs() {
+//   return fetch("https://api.thecatapi.com/v1/images/search?limit=1")
+//     .then((response) => response.json())
+//     .catch(console.log);
+// }
+
+function appendImages(images) {
+  for (const img of [{}]) {
+    const imgElement = new CatImage(img)
+    const listElement = document.createElement("li");
+    listElement.appendChild(imgElement);
+    document.body.querySelector("#list").appendChild(listElement);
+  }
+}
+
+// getCatImgs().then(appendImages);
